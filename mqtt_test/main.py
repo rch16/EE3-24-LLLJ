178575ebc4506d8a3d0fc6720a054cb1e7c46eaf
ij96 <ij96@users.noreply.github.com>
2018-02-10 17:40:30 +0000

    cw1/mqtt: Added time stamp to published data

--- mqtt_test/main.py
+++ mqtt_test/main.py
@@ -5,14 +5,26 @@ import machine
 import ujson
 import utime
 import network
+import usocket
 
 # parameters
 CLIENT_ID = machine.unique_id() # b'K\x9b\xc6\x00'
 BROKER_ADDRESS = '192.168.0.10'
-TOPIC = 'esys/LLLJ/pedometer'
+TOPIC = '/esys/LLLJ/pedometer'
 ESSID = 'EEERover'
 PASSWORD = 'exhibition'
+SEND_INTERVAL = 2       # unit: second. Time interval for publishing to MQTT
+RECONNECT_INTERVAL = 1  # unit: second. Time interval attempting reconnect to Wi-Fi
 
+# variable
+rtc = machine.RTC()                     # RTC clock
+rtc.datetime((2018,2,10,5,17,23,0,0))   # intialise
+
+# functions
+def formatted_datetime(datetime):
+    return '{:d}-{:d}-{:d} {:02d}:{:02d}:{:02d}'.format(datetime[0], datetime[1], datetime[2], datetime[4], datetime[5], datetime[6])
+
+# main program
 print('Client ID: {0}'.format(CLIENT_ID))
 
 sta_if = network.WLAN(network.STA_IF)
@@ -25,7 +37,7 @@ sta_if.active(True)
 print('Connecting to Wi-Fi...')
 while not sta_if.isconnected():
     sta_if.connect(ESSID, PASSWORD)
-    utime.sleep(1)      # wait before attempting to reconnect
+    utime.sleep(RECONNECT_INTERVAL)      # wait before attempting to reconnect
 print('Connected!')
 
 print('Connecting to MQTT broker...')
@@ -36,6 +48,10 @@ print('Connected!')
 print('Sending data...')
 while True:
     # get new data
-    data = ujson.dumps({'x':0, 'y':0, 'z':1}) # JSON data
-    client.publish(TOPIC, bytes(data, 'utf-8'))
-    print('Data published: {0}'.format(data))
+    data = {'x':0, 'y':0, 'z':1}
+    data['time'] = formatted_datetime(rtc.datetime())
+
+    payload = ujson.dumps(data) # JSON data
+    client.publish(TOPIC, bytes(payload, 'utf-8'))
+    print('Data published: {0}'.format(payload))
+    utime.sleep(SEND_INTERVAL)
