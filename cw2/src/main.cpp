31898c936cf7bf02e97575e13f4c9ad65210ec1b
ij96 <ij96@users.noreply.github.com>
2018-03-19 20:27:57 +0000

    pro: Part 1 done

--- cw2/src/main.cpp
+++ cw2/src/main.cpp
@@ -1,5 +1,8 @@
+//////////////////// Includes //////////////////////////////////////////////////
 #include "mbed.h"
+#include "Crypto.h"
 
+//////////////////// Variables /////////////////////////////////////////////////
 //Photointerrupter input pins
 #define I1pin D2
 #define I2pin D11
@@ -32,17 +35,19 @@ State   L1  L2  L3
 //Drive state to output table
 const int8_t driveTable[] = {0x12,0x18,0x09,0x21,0x24,0x06,0x00,0x00};
 
-//Mapping from interrupter inputs to sequential rotor states. 0x00 and 0x07 are not valid
+//Mapping from interrupter inputs to sequential rotor states. 0x00 and 0x07
+// are not valid
 const int8_t stateMap[] = {0x07,0x05,0x03,0x04,0x01,0x00,0x02,0x07};  
-//const int8_t stateMap[] = {0x07,0x01,0x03,0x02,0x05,0x00,0x04,0x07}; //Alternative if phase order of input or drive is reversed
+//const int8_t stateMap[] = {0x07,0x01,0x03,0x02,0x05,0x00,0x04,0x07};
+  //Alternative if phase order of input or drive is reversed
 
 //Phase lead to make motor spin
 const int8_t lead = 2;  //2 for forwards, -2 for backwards
 
-int8_t orState = 0;
-volatile int8_t intState = 0;
-volatile int8_t intStateOld = 0;
-
+//Rotor states
+int8_t orState = 0;              /* Rotot offset at motor state 0 */
+volatile int8_t intStateOld = 0; /* Motor old state. Type is volatile since
+                                    its value may change in ISR */
 //Status LED
 DigitalOut led1(LED1);
 
@@ -58,6 +63,11 @@ DigitalOut L2L(L2Lpin);
 DigitalOut L2H(L2Hpin);
 DigitalOut L3L(L3Lpin);
 DigitalOut L3H(L3Hpin);
+
+//TImers
+Timer t_bitcoin;                /* for calculating hash computation rate */
+ 
+//////////////////// Functions /////////////////////////////////////////////////
 
 //Set a given drive state
 void motorOut(int8_t driveState){
@@ -80,12 +90,12 @@ void motorOut(int8_t driveState){
     if (driveOut & 0x08) L2H = 0;
     if (driveOut & 0x10) L3L = 1;
     if (driveOut & 0x20) L3H = 0;
-    }
+}
     
 //Convert photointerrupter inputs to a rotor state
 inline int8_t readRotorState(){
     return stateMap[I1 + 2*I2 + 4*I3];
-    }
+}
 
 //Basic synchronisation routine    
 int8_t motorHome() {
@@ -99,15 +109,17 @@ int8_t motorHome() {
 
 // motor ISR
 void motorISR() {
-    intState = readRotorState();
+    int8_t intState = readRotorState();
     if (intState != intStateOld) {
         intStateOld = intState;
-        motorOut((intState-orState+lead+6)%6); //+6 to make sure the remainder is positive
+        motorOut((intState-orState+lead+6)%6); //+6 to make sure the remainder
+                                               //  is positive
     }
 }
 
-//Main
+//////////////////// Main //////////////////////////////////////////////////////
 int main() {
+    //Attach interrupt service routines to photointerrupters
     I1.rise(&motorISR);
     I1.fall(&motorISR);
     I2.rise(&motorISR);
@@ -115,17 +127,48 @@ int main() {
     I3.rise(&motorISR);
     I3.fall(&motorISR);
     
-    //int8_t orState = 0;    //Rotot offset at motor state 0
+    //Bitcoin
+    SHA256 sha;
+    const uint8_t sequence[] = {\
+        0x45,0x6D,0x62,0x65,0x64,0x64,0x65,0x64,\
+        0x20,0x53,0x79,0x73,0x74,0x65,0x6D,0x73,\
+        0x20,0x61,0x72,0x65,0x20,0x66,0x75,0x6E,\
+        0x20,0x61,0x6E,0x64,0x20,0x64,0x6F,0x20,\
+        0x61,0x77,0x65,0x73,0x6F,0x6D,0x65,0x20,\
+        0x74,0x68,0x69,0x6E,0x67,0x73,0x21,0x20,\
+        0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,\
+        0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00
+    };
+    uint64_t* key = (uint64_t*)((int)sequence + 48);
+    uint64_t* nonce = (uint64_t*)((int)sequence + 56);
+    uint8_t hash[32];
+    uint32_t hash_counter = 0;  // for calculating hash computation rate
     
-    //Initialise the serial port
+    /* Initialise the serial port */
     Serial pc(SERIAL_TX, SERIAL_RX);
-    pc.printf("Hello world!\n\r");
+    pc.printf("\n\rHello world!\n\r");
     
-    //Run the motor synchronisation
+    /* Run the motor synchronisation
+       orState is subtracted from future rotor state inputs to align rotor and
+       motor states */
     orState = motorHome();
     pc.printf("Rotor origin: %x\n\r",orState);
-    //orState is subtracted from future rotor state inputs to align rotor and motor states
     
-    //Poll the rotor state and set the motor outputs accordingly to spin the motor
-    while (1) {}
+    /* Bitcoin mining (photointerrupter may also happen in this loop) */
+    t_bitcoin.start();          // start timer
+    while (1) {
+        sha.computeHash(hash, (uint8_t*)sequence, 64);
+        if(hash[0] == 0 && hash[1] == 0)            // if nonce is found
+            printf("Nonce: 0x%016llX\n\r", *nonce);
+            
+        (*nonce)++;         // increment nonce value
+        hash_counter++;     // increment counter
+        
+        if(t_bitcoin.read() >= 1) {                 // check if 1 second passed
+            //printf("Computation rate per second: %d\n\r", hash_counter);
+            printf("Comp. rate: %d\n\r", hash_counter);// report rate per second
+            t_bitcoin.reset();  // reset timer
+            hash_counter = 0;   // reset counter
+        }
+    }
 }
